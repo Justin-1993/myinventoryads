2e258e5b9d7b37df410ad26dc2ebc40abe2fe5f1
Justin White <76507754+Justin-1993@users.noreply.github.com>
2021-01-14 23:43:04 -0800

    Fixed

--- myinventoryads/settings.py
+++ myinventoryads/settings.py
@@ -93,7 +93,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
 
 # Media Folder Settings
 
@@ -115,3 +129,4 @@ except ImportError:
         pass
 
 django_heroku.settings(locals())
+
