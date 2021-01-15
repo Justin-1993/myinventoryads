9242470b7d27ce966be7742fbacc786dc763e10b
Justin White <76507754+Justin-1993@users.noreply.github.com>
2021-01-14 22:28:52 -0800

    server settings

--- myinventoryads/settings.py
+++ myinventoryads/settings.py
@@ -92,13 +92,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'myinventoryads/static'
-]
+
 # Media Folder Settings
-MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = '/media/'
+
 
 # Messages
 
