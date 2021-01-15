184098a0ea12210ff7ed99371b66b322036b0404
Justin White <76507754+Justin-1993@users.noreply.github.com>
2021-01-14 23:00:05 -0800

    Fixed migrate error

--- myinventoryads/server_settings.py
+++ myinventoryads/server_settings.py
@@ -31,7 +31,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'myinventoryads/static'),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
