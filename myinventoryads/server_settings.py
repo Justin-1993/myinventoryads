edc04689dcc4143db310ba8d0a2cd17065324548
Justin White <76507754+Justin-1993@users.noreply.github.com>
2021-01-14 23:01:34 -0800

    Fixed migrate error

--- myinventoryads/server_settings.py
+++ myinventoryads/server_settings.py
@@ -8,7 +8,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = '_&8sv%o^883*r$+)-$5gfdg5346bm+k-4qz+e!3a79e$!c6=nv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['inventoryads.herokuapp.com']
 
