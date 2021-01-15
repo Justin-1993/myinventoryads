564b7de1fb96c9abf1406e1280799f297c576047
Justin White <76507754+Justin-1993@users.noreply.github.com>
2021-01-14 22:43:57 -0800

    server settings

--- myinventoryads/settings.py
+++ myinventoryads/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -111,3 +112,5 @@ except ImportError:
         from .server_settings import *
     except ImportError:
         pass
+
+django_heroku.settings(locals())
