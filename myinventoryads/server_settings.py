e9ef5c0df1e0ebeefc7e7ad2e9013aded4ca7064
Justin White <76507754+Justin-1993@users.noreply.github.com>
2021-01-14 22:10:45 -0800

    server settings

--- myinventoryads/server_settings.py
+++ myinventoryads/server_settings.py
@@ -14,7 +14,10 @@ ALLOWED_HOSTS = ['inventoryads.herokuapp.com']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_ROOT, '../db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'da2ocg30uggmgc',
+        'USER': 'zeyaoufzhreotp',
+        'PASSWORD': '1040fce340de8ca47c1c8aea45a43f708986091eec9a4781ab9d79bca7bf76c4',
+        'HOST': 'ec2-18-210-214-86.compute-1.amazonaws.com',
     }
 }
