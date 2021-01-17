437cc458c043e944b01d6a32b970d88bbe1e8740
Justin White <76507754+Justin-1993@users.noreply.github.com>
2021-01-16 16:01:41 -0800

    fixed minor things

--- myinventoryads/settings.py
+++ myinventoryads/settings.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import os
 import django_heroku
-import storages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +19,6 @@ INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'django.contrib.humanize',
     'membership.apps.MembershipConfig',
-    'storages'
     
 ]
 
