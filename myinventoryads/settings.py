f0dae78a2b88f323dc8c835599a051fd284e6bc2
Justin-1993 <76507754+Justin-1993@users.noreply.github.com>
2021-02-19 19:08:16 -0800

    updated

--- myinventoryads/settings.py
+++ myinventoryads/settings.py
@@ -3,7 +3,7 @@ import os
 import django_heroku
 
 
-DEBUG = True
+DEBUG = False
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
